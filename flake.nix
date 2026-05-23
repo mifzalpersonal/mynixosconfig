feaf1536ac4692e79e862fa200c997870950a7c3
mifzalpersonal <mizakigaming89@gmail.com>
2026-05-23 17:09:22 +0700

    trying out caelestia-shell, real nervous

--- flake.nix
+++ flake.nix
@@ -4,35 +4,32 @@
   inputs = {
     # Mengambil paket dari Nixpkgs unstable (paling update)
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    # Mengambil sumber paket dari Home Manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # 1. TAMBAHKAN BARIS SAKTI INI DI BLOK INPUTS
+    # Caelestia CLI / Shell Source
     caelestia.url = "github:caelestia-dots/cli";
-
-    # Mengambil sumber paket dari Home Manager
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, caelestia, ... }@inputs: {
     nixosConfigurations = {
-      # 'nixos' ini sesuai dengan hostname laptop lu saat ini
+      # 'nix' sesuai dengan hostname laptop lu saat ini
       nix = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-	  {
-    	     home-manager.extraSpecialArgs = { inherit inputs caelestia; }; # <--- Pastikan ada 'caelestia' atau 'inputs' di sini
-  	  }
 
           # Integrasikan Home Manager ke dalam Flake sistem
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            
+            # Ini tempat ngirim variabel 'caelestia' ke dalam home.nix dengan bener
+            home-manager.extraSpecialArgs = { inherit inputs caelestia; };
+            
             home-manager.users.ciel = import ./home.nix;
           }
         ];
