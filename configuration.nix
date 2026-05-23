8ff6f44f638a29fdf08c5d864c65e0858b4e0e76
mifzalpersonal <mizakigaming89@gmail.com>
2026-05-23 20:13:42 +0700

    fixing indentation on line 116

--- configuration.nix
+++ configuration.nix
@@ -103,7 +103,7 @@
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-   environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      neovim
@@ -116,7 +116,6 @@
      dolphin
      steam
      discord
-
    ];
 
    environment.shellAliases = {
