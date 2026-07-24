1f55029f70cd272a6a7c8103b349dac18b67df09
mifzalpersonal <mizakigaming89@gmail.com>
2026-07-24 23:15:19 +0700

    changing cachy kernel to precompiled one since compiling takes a lot of time

--- configuration.nix
+++ configuration.nix
@@ -192,15 +192,21 @@
 
   # --------------CACHY KERNEL-------------------
   nixpkgs.overlays = [
-    inputs.nix-cachyos-kernel.overlays.default
+    inputs.nix-cachyos-kernel.overlays.pinned
   ];
 
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-x86_64-v3;
 
-  #nix.settings = {
-  #  substituters = [ "https://attic.xuyh0120.win/lantian" ];
-  #  trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
-  #};
+  nix.settings = {
+    substituters = [ 
+      "https://cache.nixos.org" 
+      "https://attic.xuyh0120.win/lantian" 
+    ];
+    trusted-public-keys = [ 
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" 
+    ];
+  };
   # --------------------------------------
 
 
