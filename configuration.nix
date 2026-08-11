81b260de137758eec26088216d8723f1b443a52c
mifzalpersonal <mizakigaming89@gmail.com>
2026-08-11 10:50:26 +0700

    naekin start charge ke 95% biar kalo dilepas dikit langsung cas, abis tu ngilangin opacity dan blur buat window biasa

--- configuration.nix
+++ configuration.nix
@@ -218,11 +218,11 @@
     enable = true;
     settings = {
       # Mulai ngecas kalau batre di bawah 75%, stop ngecas di 80%
-      START_CHARGE_THRESH_BAT0 = 90;
+      START_CHARGE_THRESH_BAT0 = 95;
       STOP_CHARGE_THRESH_BAT0 = 98;
       
       # Karena T480 punya dual battery (Bridge Battery System), atur juga BAT1
-      START_CHARGE_THRESH_BAT1 = 90;
+      START_CHARGE_THRESH_BAT1 = 95;
       STOP_CHARGE_THRESH_BAT1 = 98;
 
       CPU_SCALING_GOVERNOR_ON_AC = "powersave"; # Atau performance 
