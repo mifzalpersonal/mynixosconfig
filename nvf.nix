283971e2efc48e8e6a7a4e274f4e3770fefd2700
mifzalpersonal <mizakigaming89@gmail.com>
2026-08-30 23:31:40 +0700

    nvf docs, look it up. the right one is the previous one

--- nvf.nix
+++ nvf.nix
@@ -8,6 +8,20 @@
       viAlias = true;
       vimAlias = true;
 
+      # 1. Tombol Spasi Cheatsheet (which-key)
+      binds.whichKey.enable = true;
+
+      # 2. File Selector & Picker (FZF atau Telescope)
+      # Pilih salah satu (atau keduanya):
+      fzf.cmake-passthrough = false; # FZF Lua Integration
+      fzf.enable = true;             # Pakai fzf-lua
+
+      # 3. Buffer Line (Tab Bar / Buffer Selector)
+      tabline.nvimBufferline.enable = true;
+
+      # File Tree / File Explorer Sidebar (NvimTree)
+      filetree.nvimTree.enable = true;
+      
       theme = {
         enable = true;
         name = "catppuccin";
